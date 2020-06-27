61c907a99c37c7eed3c928db0d6da6c47b7581d0
DanL12186 <DanL12186@users.noreply.github.com>
2020-06-27 18:50:04 -0500

    remove needless code from welcome component

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-welcome',
@@ -11,19 +11,10 @@ export class WelcomeComponent implements OnInit {
   backendData : Object;
   backendMessage = '';
 
-  constructor(private http: HttpClient) {}
+  constructor(private titleService: Title) {}
 
   ngOnInit(): void {
-    this.http.get("http://localhost:3002/").subscribe(
-      response => this.handleResponse(response), 
-      error    => "error!"
-    );
-  }
-
-  handleResponse(response) {
-    console.log(response)
-    this.backendData = response;
-    this.backendMessage = response.what
+    this.titleService.setTitle('Coin Catalogue');
   }
 
 }
